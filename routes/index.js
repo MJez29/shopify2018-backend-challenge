d8607e2ccee1e43b4e3431915ed89bf4a9c7064d
Michal Jez <Michal.G.Jez@gmail.com>
2018-09-23 17:11:35 -0400

    Added error handlers

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,21 @@ router.use(morgan('dev'));
 // Parse application/json
 router.use(bodyParser.json());
 
+// Hello world
 router.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
 router.use('/shops', require('./shops'));
+
+router.use('*', (req, res) => {
+  res.status(404).json({ error: 'Path not found' });
+});
+
+// Default error-handler
+router.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: 'An unexpected error occured' });
+})
 
 module.exports = router;
